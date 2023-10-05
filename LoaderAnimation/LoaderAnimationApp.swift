c0f3c7e2838461ad9a9451ac9c19aa56e8e32324
Elaidzha1940 <eli080293@gmail.com>
2023-10-06 02:17:59 +0300

    LoaderAnimation

---
+++ LoaderAnimation/LoaderAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoaderAnimationApp.swift
+//  LoaderAnimation
+//
+//  Created by Elaidzha Shchukin on 06.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct LoaderAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
