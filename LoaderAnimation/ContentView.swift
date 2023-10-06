2586548c3825c639eabb5103613bbcc9a51e4051
Elaidzha1940 <eli080293@gmail.com>
2023-10-06 03:16:25 +0300

    Added animate

--- LoaderAnimation/ContentView.swift
+++ LoaderAnimation/ContentView.swift
@@ -35,23 +35,28 @@ struct LoaderCircle: View {
     @State var animate2 = false
     @State var animate3 = false
     @State var animate4 = false
-
+    
     var body: some View {
         
         ZStack {
             
             Rectangle()
-                .
-            RoundedRectangle(cornerRadius: isAnimated ? 50 : 5)
-                .fill(isAnimated ? Color.black : Color.red)
-                .frame(
-                    width: isAnimated ? 100 : 300,
-                    height: isAnimated ? 100 : 300
-                )
-                .rotationEffect(Angle(degrees: isAnimated ? 360 : 0))
-                //.offset(y: isAnimated ? 300 : 0)
-//                .animation(Animation.linear(duration: 1.5).repeatForever(autoreverses: true))
-            Spacer()
-        }
+                .foregroundColor(.white)
+                .cornerRadius(isAnimated ? 60 : 0)
+                .frame(width: 70, height: 70, alignment: .center)
+                .foregroundColor(.clear)
+                .overlay(isAnimated ?
+                         RoundedRectangle(cornerRadius: 50)
+                    .stroke(Color.black, lineWidth: 10) :
+                            RoundedRectangle(cornerRadius: 0)
+                    .stroke(Color.black, lineWidth: 10))
+                .opacity(isAnimated ? 0.5 : 1)
+                .scaleEffect(isAnimated ? 0.4 : 1)
+                .rotationEffect(Angle(degrees: isAnimated ? 90 : -90))
+                .animation(Animation.easeInOut(duration: 2).repeatForever(autoreverses: true))
+                .onAppear {
+                    self.animate.toggle()
+                }
+        }.offset(y: 80)
     }
 }
